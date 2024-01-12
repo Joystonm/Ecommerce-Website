4c28497db74e45cb7f918d2c4f8c9bdd2d632f06
Dineshsut07 <120447913+Dineshsut07@users.noreply.github.com>
2024-01-12 10:07:44 +0530

    website

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import '../styles/Navbar.css'
+import {BsBagCheck} from 'react-icons/bs';
+import {Link} from 'react-router-dom'
+const Nav = () => {
+  return (
+    <>
+    <div className='main-header'>
+        <div className='container'>
+            <div className='logo'>
+                <img src='./images/OIG_mcR5i1qxke-removebg-preview-transformed.png' alt='logo' ></img>
+                <p className='logo-header'>DEALHUB</p>
+            </div>
+
+            <div className='search-box'>
+                <input type='text' placeholder='Search'></input>
+                <button>Search</button>
+            </div>
+
+            <div className='header'>
+                <div className='container'>
+                    <div className='nav'>
+                        <ul>
+                            <li>
+                                <Link to='/' className='link'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/product' className='link'>Product</Link>
+                            </li>
+                            <li>
+                                <Link to='sign-in' className='link'>Sign-in</Link>
+                            </li>
+                            <li>
+                                <Link to='sign-in' className='link'>Add To cart</Link>
+                            </li>
+                            <div className='icon'>
+                 <BsBagCheck/>
+            </div>
+                        </ul>
+                       
+                    </div>
+                </div>
+            </div>       
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Nav
